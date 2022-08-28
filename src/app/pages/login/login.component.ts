d1fca2b3e077e730181f5e2048aca62022eec8e4
ziedlahbib <lahbibmedzied@gmail.com>
2022-08-28 20:39:18 +0100

    update

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -27,7 +27,7 @@ export class LoginComponent implements OnInit {
       this.invalidLogin = false;
       this.loginSuccess = true;
       this.successMessage = 'Login Successful.';
-      this.router.navigate(['/Cherchermeilleurassurance']);
+      this.router.navigate(['/home']);
     }, () => {
       this.invalidLogin = true;
       this.loginSuccess = false;
